2b24e04a10e9e1f80fd1cad7a97481506af75069
Paul Gelot <pgelot@gmail.com>
2018-08-09 01:43:08 -0400

    slow start

---
+++ lunch_gang.py
@@ -0,0 +1,55 @@
+import csv
+import pprint
+
+nameslistopen = open('Names.csv')
+nameslistdata = csv.reader(nameslistopen)
+
+nameslist = []
+
+for name in nameslistdata:
+    nameslist.append(column)
+# print(group1)
+
+group1 = []
+for name in  len(namelist):
+    while namelist[name] 
+# group1 = [n for n in nameslist if [n] ==]
+
+for name in nameslist:
+    if nameslist[names] % 6 == 0:
+        nameslist
+
+
+
+# def chunks(l, n):
+#     """Yield successive n-sized chunks from l."""
+#     for i in range(0, len(l), n):
+#         yield l[i:i + n]
+
+# nameslist = ''.join(nameslist)
+# pprint.pprint(chunks(range(nameslist[0],nameslist[89]), 6))
+
+# pprint.pprint(group1(chunks(range(10, 75), 10)))
+
+
+# import csv
+
+# f = open('attendees1.csv')
+# csv_f = csv.reader(f)
+
+# attendee_emails1 = []
+
+# for row in csv_f:
+#   attendee_emails1.append(row[2])
+
+
+# f = open('attendees2.csv')
+# csv_f = csv.reader(f)
+
+# attendee_emails2 = []
+
+# for row in csv_f:
+#   attendee_emails2.append(row[2])
+
+# print attendee_emails1
+# print attendee_emails2
